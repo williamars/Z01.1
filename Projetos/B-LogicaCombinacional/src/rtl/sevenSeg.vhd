181350149e3e83e7bdae4ccae55345060bdbca68
borg <joaogcfa@al.insper.edu.br>
2020-03-05 09:03:46 -0300

    sevenSeg.vhd passou no teste

--- Projetos/B-LogicaCombinacional/src/rtl/sevenSeg.vhd
+++ Projetos/B-LogicaCombinacional/src/rtl/sevenSeg.vhd
@@ -9,22 +9,22 @@ end entity;
 
 architecture arch of sevenSeg is
 begin
-	leds <= "0000001" when (bcd = '0000') else; -- 0
-	"1001111" when (bcd = '0001') else; -- 1
-	"0010010" when (bcd = '0010') else; -- 2
-	"0000110" when (bcd = '0011') else; -- 3
-	"1001100" when (bcd = '0100') else; -- 4
-	"0100100" when (bcd = '0101') else; -- 5
-	"0100000" when (bcd = '0110') else; -- 6
-	"0001111" when (bcd = '0111') else; -- 7
-	"0000000" when (bcd = '1000') else; -- 8
-	"0000100" when (bcd = '1001') else; -- 9 
-	"0000010" when (bcd = '1010') else; -- a
-	"1100000" when (bcd = '1011') else; -- b
-	"0110001" when (bcd = '1100') else; -- c
-	"1000010" when (bcd = '1101') else; -- d 
-	"0110000" when (bcd = '1110') else; -- E
-	"0111000" when (bcd = '1111') else; -- F
+	leds <= "0000001" when (bcd = "0000") else -- 0
+	"1001111" when (bcd = "0001") else -- 1
+	"0010010" when (bcd = "0010") else -- 2
+	"0000110" when (bcd = "0011") else -- 3
+	"1001100" when (bcd = "0100") else -- 4
+	"0100100" when (bcd = "0101") else -- 5
+	"0100000" when (bcd = "0110") else -- 6
+	"0001111" when (bcd = "0111") else -- 7
+	"0000000" when (bcd = "1000") else -- 8
+	"0000100" when (bcd = "1001") else -- 9 
+	"0000010" when (bcd = "1010") else -- a
+	"1100000" when (bcd = "1011") else -- b
+	"0110001" when (bcd = "1100") else -- c
+	"1000010" when (bcd = "1101") else -- d 
+	"0110000" when (bcd = "1110") else -- E
+	"0111000"; -- F
 
 
 
