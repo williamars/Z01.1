c3dde2f816fc2a91bb0ee2bfc888d5e8b746bbfb
MatFreitas <matheusfs2@al.insper.edu.br>
2020-03-25 19:43:50 -0300

    FullAdder testado e funcionando

--- Projetos/C-UnidadeLogicaAritmetica/src/rtl/FullAdder.vhd
+++ Projetos/C-UnidadeLogicaAritmetica/src/rtl/FullAdder.vhd
@@ -18,8 +18,38 @@ architecture rtl of FullAdder is
   -- Aqui declaramos sinais (fios auxiliares)
   -- e componentes (outros módulos) que serao
   -- utilizados nesse modulo.
+component FullAdder1
+    port
+    (
+        -- Input ports
+        a   : in  std_logic;
+        b   : in  std_logic;
+        c: in std_logic;
+
+        -- Output ports
+        soma   : out std_logic;
+        vaium: out std_logic
+    );
+end component;
 
 begin
   -- Implementação vem aqui!
+	soma <= '0' when a='0' and b='0' and c='0' else
+		'1' when a='0' and b='0' and c='1' else          
+		'1' when a='0' and b='1' and c='0' else
+		'0' when a='0' and b='1' and c='1' else
+		'1' when a='1' and b='0' and c='0' else
+ 		'0' when a='1' and b='0' and c='1' else
+		'0' when a='1' and b='1' and c='0' else
+		'1';
+
+	vaium <= '0' when a='0' and b='0' and c='0' else
+		 '0' when a='0' and b='0' and c='1' else          
+		 '0' when a='0' and b='1' and c='0' else
+		 '1' when a='0' and b='1' and c='1' else
+		 '0' when a='1' and b='0' and c='0' else
+ 		 '1' when a='1' and b='0' and c='1' else
+		 '1' when a='1' and b='1' and c='0' else
+		 '1';
 
 end architecture;
