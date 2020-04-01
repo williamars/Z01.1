6c56ddce2dca59271001849e98971dc61c4a8c43
MatFreitas <matheusfs2@al.insper.edu.br>
2020-04-01 15:34:51 -0300

    Lab10

--- Projetos/D-LogicaSequencial/src/rtl/FlipFlopD.vhd
+++ Projetos/D-LogicaSequencial/src/rtl/FlipFlopD.vhd
@@ -2,29 +2,48 @@
 -- by Luciano Soares
 -- FlipFlopD.vhd
 
+ 
+
 library ieee;
 use ieee.std_logic_1164.all;
 
+ 
+
 entity FlipFlopD is
-	port(
-		clock:  in std_logic;
-		d:      in std_logic;
-		clear:  in std_logic;
-		preset: in std_logic;
-		q:      out std_logic := '0'
-	);
+    port(
+        clock:  in std_logic;
+        d:      in std_logic;
+        clear:  in std_logic;
+        preset: in std_logic;
+        q:      out std_logic := '0'
+    );
 end entity;
+
+ 
 
 architecture arch of FlipFlopD is
 
+ 
+
 begin
 
-	process(clock, clear) begin
-		if (clear = '1') then
-			q <='0';
-		elsif (rising_edge(CLOCK)) then
-			q<=D;
-		end if;
+ 
+
+    process(clock, clear, preset) begin
+
+ 
+
+        if (clear = '1') then
+            q <='0';
+        elsif (preset ='1' ) then 
+            q<= '1';
+        elsif (rising_edge(CLOCK)) then
+            q<=D;
+        end if;
   end process;
+
+ 
+
+ 
 
 end architecture;
