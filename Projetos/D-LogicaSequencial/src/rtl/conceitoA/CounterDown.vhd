3a8618ba28d2cb1df26a6a67dd381ffe9cbe4890
MatFreitas <matheusfs2@al.insper.edu.br>
2020-04-07 14:57:15 -0300

    ajuste no teste do FlippFlopT e ouitrasimplementações

--- Projetos/D-LogicaSequencial/src/rtl/conceitoA/CounterDown.vhd
+++ Projetos/D-LogicaSequencial/src/rtl/conceitoA/CounterDown.vhd
@@ -13,6 +13,50 @@ end entity;
 
 architecture arch of CounterDown is
 
+	component FlipFlopT is
+		port(
+		clock:  in std_logic;
+		t:      in std_logic;
+		q:      out std_logic:= '0';
+		notq:   out std_logic:= '1'
+	);
+	end component;
+
+signal q0, q1, q2: std_logic;
+
+
 begin
+
+	
+	t0: FlipFlopT port map(
+		clock => clock,
+		t =>  '1', 	
+		q => q0
+		);
+
+	t1: FlipFlopT port map(
+		clock => q0,
+		t => '1',
+		q => q1
+	);
+
+	t2: FlipFlopT port map(
+		clock => q1,
+		t => '1',
+		q => q2
+	);
+
+	q <= q2 & q1 & q0; 
+	
+	process(clock) begin
+
+        if (rising_edge(clock)) then
+            q <= ;
+        end if;
+
+    end process;
+	
+	
+	
 
 end architecture;
--- Projetos/D-LogicaSequencial/tests/tst/tb_CounterDown.vhd
+++ Projetos/D-LogicaSequencial/tests/tst/tb_CounterDown.vhd
@@ -34,8 +34,46 @@ begin
     test_runner_setup(runner, runner_cfg);
 
     -- IMPLEMENTE AQUI!
+   
+    -- Teste: 0
+	  q(2) <= '1'; q(1) <= '1'; q(0) <= '0'; 
     wait until clk'event and clk='0';
-		assert(Q = "111")  report "Precisa fazer os testes" severity error;
+		assert(Q= "110")     report "Falha em teste: 0" severity error;
+
+    -- Teste: 1
+	  q(2) <= '1'; q(1) <= '0'; q(0) <= '1'; 
+    wait until clk'event and clk='0';
+		assert(Q= "101")     report "Falha em teste: 1" severity error;
+
+    -- Teste: 2
+	  q(2) <= '1'; q(1) <= '0'; q(0) <= '0'; 
+    wait until clk'event and clk='0';
+		assert(Q= "100")     report "Falha em teste: 2" severity error;
+
+    -- Teste: 3
+	  q(2) <= '0'; q(1) <= '1'; q(0) <= '1'; 
+    wait until clk'event and clk='0';
+		assert(Q= "011")     report "Falha em teste: 3" severity error;
+
+    -- Teste: 4
+	  q(2) <= '0'; q(1) <= '1'; q(0) <= '0'; 
+    wait until clk'event and clk='0';
+		assert(Q= "010")     report "Falha em teste: 4" severity error;
+
+    -- Teste: 5
+	  q(2) <= '0'; q(1) <= '0'; q(0) <= '1'; 
+    wait until clk'event and clk='0';
+		assert(Q= "001")     report "Falha em teste: 5" severity error;
+
+    -- Teste: 6
+	  q(2) <= '0'; q(1) <= '0'; q(0) <= '0'; 
+    wait until clk'event and clk='0';
+		assert(Q= "000")     report "Falha em teste: 6" severity error;
+
+    -- Teste: 7
+    q(2) <= '1'; q(1) <= '1'; q(0) <= '1';
+    wait until clk'event and clk='0';
+		assert(Q = "111")  report "Falha em teste 7" severity error;
 
 
     -- finish
