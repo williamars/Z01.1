4fbd8dc2836c1c6a72200a7e106f8c25e21c4b5b
Rafael Corsi <rafael.corsi@insper.edu.br>
2020-04-22 14:50:28 -0300

    melhorado script de atualizacao

--- updateZ01tools.sh
+++ updateZ01tools.sh
@@ -16,12 +16,11 @@ Z01_PATH=$HOME/Z01-Tools/
 Z01_TOOLS_URL=https://github.com/insper/Z01-tools
 if [ ! -d "$Z01_PATH" ]; then
     echo "Instalando Z01-1 tools"
-    git clone $Z01_TOOLS_URL $Z01_PATH
 else
-    echo "Buscando por atualizações no Z01-1 tools"
-    git -C $Z01_PATH stash
-    git -C $Z01_PATH checkout master
-    git -C $Z01_PATH pull origin master
+    echo "Atualizando Z01-1 tools"
+    rm -rf $Z01_PATH
 fi
+
+git clone $Z01_TOOLS_URL $Z01_PATH
 
 wait
